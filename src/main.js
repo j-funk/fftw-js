07b26283ebd7db4738e392142f4a80219478f7f9
Dean Shaff <dshaff@swin.edu.au>
2019-01-21 14:18:20 +1100

    updated to explicitly include cwrap and ccall, in addition to other FFTW functions. Added complex to complex 1d transform, and included complex to complex 2d transform

--- src/main.js
+++ src/main.js
@@ -1,37 +1,93 @@
 "use strict";
 
-var FFTWModule = require('./FFTW');
+var FFTWModule = require('./FFTW.js');
 var fftwModule = FFTWModule({});
 
-var fftwf_plan_dft_r2c_1d = fftwModule.cwrap(
+const fftwf_malloc = fftwModule.cwrap(
+  'fftwf_malloc', 'number', ['number']
+)
+
+const fftwf_plan_dft_r2c_1d = fftwModule.cwrap(
     'fftwf_plan_dft_r2c_1d', 'number', ['number', 'number', 'number', 'number']
 );
 
-var fftwf_plan_dft_c2r_1d = fftwModule.cwrap(
+const fftwf_plan_dft_c2r_1d = fftwModule.cwrap(
     'fftwf_plan_dft_c2r_1d', 'number', ['number', 'number', 'number', 'number']
 );
 
-var fftwf_plan_r2r_1d = fftwModule.cwrap(
+const fftwf_plan_r2r_1d = fftwModule.cwrap(
     'fftwf_plan_r2r_1d', 'number', ['number', 'number', 'number', 'number']
 );
 
-var fftwf_execute = fftwModule.cwrap(
+// fftw_plan fftw_plan_dft_1d(int n, fftw_complex *in, fftw_complex *out,
+//                            int sign, unsigned flags);
+const fftwf_plan_dft_1d = fftwModule.cwrap(
+  'fftwf_plan_dft_1d', 'number', ['number', 'number', 'number', 'number', 'number']
+)
+
+// fftw_plan fftw_plan_dft_2d(int n0, int n1,
+//                            fftw_complex *in, fftw_complex *out,
+//                            int sign, unsigned flags);
+const fftwf_plan_dft_2d = fftwModule.cwrap(
+  'fftwf_plan_dft_2d', 'number', ['number', 'number', 'number', 'number', 'number', 'number']
+)
+
+
+const fftwf_execute = fftwModule.cwrap(
     'fftwf_execute', 'void', ['number']
 );
 
-var fftwf_destroy_plan = fftwModule.cwrap(
+const fftwf_destroy_plan = fftwModule.cwrap(
     'fftwf_destroy_plan', 'void', ['number']
 );
 
-var FFTW_ESTIMATE = (1 << 6);
-var FFTW_R2HC = 0;
-var FFTW_HC2R = 1;
+const fftwf_free = fftwModule.cwrap(
+  'fftwf_free', 'void', ['number']
+)
+
+const FFTW_ESTIMATE = (1 << 6);
+const FFTW_R2HC = 0;
+const FFTW_HC2R = 1;
+const FFTW_FORWARD = -1
+const FFTW_BACKWARD = 1
+
+function FFTC2C (size) {
+
+  this.size = size
+  this.c0ptr = fftwf_malloc(2*4*size + 2*4*size)
+  this.c1ptr = this.c0ptr
+
+  this.c0 = new Float32Array(fftwModule.HEAPU8.buffer, this.c0ptr, 2*size)
+  this.c1 = new Float32Array(fftwModule.HEAPU8.buffer, this.c1ptr, 2*size)
+
+  this.fplan = fftwf_plan_dft_1d(size, this.c0ptr, this.c1ptr, FFTW_FORWARD, FFTW_ESTIMATE)
+  this.iplan = fftwf_plan_dft_1d(size, this.c1ptr, this.c0ptr, FFTW_BACKWARD, FFTW_ESTIMATE)
+
+  this.forward = function(cpx) {
+      this.c0.set(cpx);
+      fftwf_execute(this.fplan);
+      return new Float32Array(fftwModule.HEAPU8.buffer, this.c1ptr, this.size)
+  }
+
+  this.inverse = function(cpx) {
+      this.c1.set(cpx);
+      fftwf_execute(this.iplan);
+      return new Float32Array(fftwModule.HEAPU8.buffer, this.c0ptr, this.size)
+  }
+
+  this.dispose = function() {
+      fftwf_destroy_plan(this.fplan)
+      fftwf_destroy_plan(this.iplan)
+      fftwf_free(this.c0ptr)
+  }
+
+}
 
 
-function FFT(size) {
+function FFTR2C (size) {
 
     this.size = size;
-    this.rptr = fftwModule._malloc(size*4 + (size+2)*4);
+    this.rptr = fftwf_malloc(size*4 + (size+2)*4);
     this.cptr = this.rptr + size*4;
     this.r = new Float32Array(fftwModule.HEAPU8.buffer, this.rptr, size);
     this.c = new Float32Array(fftwModule.HEAPU8.buffer, this.cptr, size+2);
@@ -54,14 +110,14 @@ function FFT(size) {
     this.dispose = function() {
         fftwf_destroy_plan(this.fplan);
         fftwf_destroy_plan(this.iplan);
-        fftwModule._free(this.rptr);
+        fftwf_free(this.rptr);
     }
 }
 
 
-function RFFT(size) {
+function FFTR2R(size) {
     this.size = size;
-    this.rptr = fftwModule._malloc(size*4 + size*4);
+    this.rptr = fftwf_malloc(size*4 + size*4);
     this.cptr = this.rptr;
     this.r = new Float32Array(fftwModule.HEAPU8.buffer, this.rptr, size);
     this.c = new Float32Array(fftwModule.HEAPU8.buffer, this.cptr, size);
@@ -84,11 +140,12 @@ function RFFT(size) {
     this.dispose = function() {
         fftwf_destroy_plan(this.fplan);
         fftwf_destroy_plan(this.iplan);
-        fftwModule._free(this.rptr);
+        fftwf_free(this.rptr);
     }
 }
 
 module.exports = {
-    FFT: FFT,
-    RFFT: RFFT
+    FFTC2C: FFTC2C,
+    FFTR2C: FFTR2C,
+    FFTR2R: FFTR2R
 };
--- test/fft.spec.js
+++ test/fft.spec.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 var chai = require('chai');
 
 var FFTW = require('../src/main');
@@ -21,11 +24,32 @@ function getMiscRealBuffer(size) {
     return result;
 }
 
+function getMiscComplexBuffer (size) {
+  var result = new Float32Array(2*size)
+  for (var i=0; i<result.length; i++) {
+    result[i] = Math.random()
+  }
+  return result
+}
+
 describe('fftw-js', function() {
+
+    it('should successfully transform and invert complex input', function () {
+      var size = 2048
+      var randomComplex = getMiscComplexBuffer(size)
+      var fftc2c = new FFTW.FFTC2C(size)
+      var forward = fftc2c.forward(randomComplex)
+      var backward = fftc2c.inverse(forward)
+      var backwardScaled = scaleTransform(backward, size)
+      for(var i = 0; i < size; i++) {
+          chai.expect(randomComplex[i]).to.be.closeTo(backwardScaled[i], 0.0000005);
+      }
+      fftc2c.dispose();
+    })
 
     it('should successfully transform and invert real valued input buffer', function() {
         var size = A2_1024.length;
-        var fftr = new FFTW.RFFT(size);
+        var fftr = new FFTW.FFTR2R(size);
         var transform = fftr.forward(A2_1024);
         var transScaled = scaleTransform(transform, size);
         var a2_again = fftr.inverse(transScaled);
@@ -41,7 +65,7 @@ describe('fftw-js', function() {
     it('should successfully transform and invert non-power-of-2 buffers', function() {
         var non2PowSize = 1536;  // 1.5 times test buffer size
         var buffer = getMiscRealBuffer(non2PowSize);
-        var fftr = new FFTW.RFFT(non2PowSize);
+        var fftr = new FFTW.FFTR2R(non2PowSize);
         var transform = fftr.forward(buffer);
         var transScaled = scaleTransform(transform, non2PowSize);
         var backAgain = fftr.inverse(transScaled);
